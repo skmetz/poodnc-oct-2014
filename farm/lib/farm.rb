3a8ac1d69115c2bc7b72b00755f72f3e0e304804
Sandi Metz <skmetz@gmail.com>
2014-08-19 11:51:23 -0400

    Revert last commit.

--- farm/lib/farm.rb
+++ farm/lib/farm.rb
@@ -12,20 +12,12 @@ class Farm
   end
 
   def verse(animal)
-    if animal.nil?
-      species = "<silence>"
-      sound   = "<silence>"
-    else
-      species = animal.species
-      sound   = animal.sound
-    end
-
     "Old MacDonald had a farm, E-I-E-I-O,\n" +
-    "And on that farm he had #{species.articlize}, E-I-E-I-O,\n" +
-    "With #{sound.articlize} #{sound} here " +
-      "and #{sound.articlize} #{sound} there,\n" +
-    "Here #{sound.articlize}, there #{sound.articlize}, " +
-      "everywhere #{sound.articlize} #{sound},\n" +
+    "And on that farm he had #{animal.species.articlize}, E-I-E-I-O,\n" +
+    "With #{animal.sound.articlize} #{animal.sound} here " +
+      "and #{animal.sound.articlize} #{animal.sound} there,\n" +
+    "Here #{animal.sound.articlize}, there #{animal.sound.articlize}, " +
+      "everywhere #{animal.sound.articlize} #{animal.sound},\n" +
     "Old MacDonald had a farm, E-I-E-I-O."
   end
 end
