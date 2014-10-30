5fec776fbe66f0b55292edd157f74fd08a608f8a
Katrina Owen <katrina.owen@gmail.com>
2014-10-30 08:30:12 -0400

    Revert "Name the first difference"
    This reverts commit 3eff71b9ddc0605bd20fd1d7eef2009f9ea1b9a1.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -14,17 +14,9 @@ class Bottles
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
-    end
-  end
-
-  def container(count)
-    if count == 1
-      "bottle"
-    else
-      "bottles"
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
     end
   end
 end
